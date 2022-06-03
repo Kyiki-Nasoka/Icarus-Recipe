cbaebbe9b20f46d27c99e709b55f364a9ebf5c3d
Kyiki-Nasoka <60371271+Kyiki-Nasoka@users.noreply.github.com>
2022-06-03 17:36:47 -0400

    Update ingredient_calculator.py
    Corrected JSON import and added new output

--- Icarus_Recipe/ingredient_calculator.py
+++ Icarus_Recipe/ingredient_calculator.py
@@ -1,16 +1,16 @@
 import PySimpleGUI as sg
 import json
 
-with open("recipes.json", "r") as infile:
+with open("Icarus_Recipe/recipes.json", "r") as infile:
     recipes = json.load(infile)
 
 
 layout = [
     [sg.Text("Target Item"), sg.Input(key="ITEM_TARGET")],
     [sg.Text("Count to make"), sg.Input(key="ITEM_COUNT")],
-    [sg.Button("Calculate", key="CALCULATE")],
+    [sg.Button("Calculate", key="CALCULATE"), sg.Button("Debug", key="DEBUG")],
     [sg.Text("Nothing to Display", key="OUTPUT")],
-    [sg.Button("debug", key = "DEBUG")]
+    [sg.Text("", key = "DEFAULTOUTPUT")]
 ]
 
 window = sg.Window("Resource Calculator", layout)
@@ -23,39 +23,45 @@ def main() -> None:
             case sg.WIN_CLOSED:
                 break
             case "CALCULATE":
-                target = values["ITEM_TARGET"]
-                count = values["ITEM_COUNT"]
-                window["OUTPUT"].update(my_format(get_base_item_total(target, count)))
+                target = values["ITEM_TARGET"].lower()
+                count = int(values["ITEM_COUNT"])
+                window["OUTPUT"].update(my_format_two(get_craft_tree(target, count)))
             case "DEBUG":
-                print(recipes)
+                target = "rifle round"
+                count = 5
+                print("-----recipes-----\n",recipes, "\n-----End Recipe-----\n")
+                print("-----craft tree-----\n",get_craft_tree(target, count), "\n-----end craft tree\n")
+                print("-----output-----\n",my_format_two(get_craft_tree(target, count)),"\n-----end output-----\n")
 
     
     window.close()
 
 #for each key in recipe list, check if it's value is in the recipes. If so, then make the entry for that item. Otherwise, default format
-def get_base_item_total(tgt, total) -> dict():
-    recipe_total = dict()
+def get_craft_tree(tgt, total) -> dict():
+    recipe_tree = dict()
     for k, v in recipes[tgt].items():
         if k in recipes:
-            recipe_total[k] = get_base_item_total(k, int(total)*v)
+            recipe_tree[k] = get_craft_tree(k, int(total)*v)
         else:
-            recipe_total[k] = v*int(total)
-    return recipe_total
+            recipe_tree[k] = v*int(total)
+    return recipe_tree
 
-#formatting top level on left, each indented recipe preceeded by an additional "-"
-def my_format(tgt_dict, depth = 0) -> str:
-    output = ""
-    for k, v in tgt_dict.items():
+def my_format_two(tgt: dict) -> dict:
+    output = dict()
+    for k, v in tgt.items():
         if isinstance(v, dict):
-            intermediate = depth*"-"
-            interm2 = f"{intermediate}{k}\n" + my_format(v, depth + 1)
-            output = output + interm2
+            temp_dict = my_format_two(v)
+            for i, j in temp_dict.items():
+                if i in output:
+                    output[i] += j
+                else:
+                    output[i] = j
         else:
-            intermediate = depth*"-"
-            return f"{intermediate}{k}: {v}\n"
+            if k in output:
+                output[k] += v
+            else:
+                output[k] = v
     return output
-
-
 
 
 if __name__ == "__main__":
