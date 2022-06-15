4133a08747960f034a130c3cf7c4416ce27da1f3
Kyiki-Nasoka <60371271+Kyiki-Nasoka@users.noreply.github.com>
2022-06-15 06:13:36 -0400

    Prints basic materials, functional
    Does not format output well. Requires restart to add ingredient

--- Icarus_Recipe/ingredient_calculator.py
+++ Icarus_Recipe/ingredient_calculator.py
@@ -4,14 +4,16 @@ import json
 with open("Icarus_Recipe/recipes.json", "r") as infile:
     recipes = json.load(infile)
 
+def default_layout() -> None:
+    return [
+        [sg.Text("Target Item"), sg.Input(key="ITEM_TARGET")],
+        [sg.Text("Count to make"), sg.Input(key="ITEM_COUNT")],
+        [sg.Button("Calculate", key="CALCULATE")],
+        [sg.Text("Output", key="OUTPUT")],
+        [sg.Button("Add Recipe", key="ADDRECIPE"), sg.Button("Save Recipe and close", key="SAVE")]
+    ]
 
-layout = [
-    [sg.Text("Target Item"), sg.Input(key="ITEM_TARGET")],
-    [sg.Text("Count to make"), sg.Input(key="ITEM_COUNT")],
-    [sg.Button("Calculate", key="CALCULATE")],
-    [sg.Text("Output", key="OUTPUT")],
-    [sg.Button("Add Recipe", key="ADDRECIPE"), sg.Button("Save Recipe and close", key="SAVE")]
-]
+layout = default_layout()
 
 window = sg.Window("Resource Calculator", layout)
 
@@ -33,6 +35,7 @@ def main() -> None:
                     window["OUTPUT"].update(f"{target} not found in recipe list")
                     continue
                 output = get_base_ingredients(get_craft_tree(target, int(count)))
+                print(get_craft_tree(target, int(count)))
                 print_result(output)
             case "ADDRECIPE":
                 window.extend_layout(window, [[sg.Text("Ingredient:"), sg.I(key = f'-IN-{i}'), sg.Text("Count:"), sg.I(key = f"-REQUIRED-{i}")]])
@@ -44,6 +47,7 @@ def main() -> None:
                     recipes[target][values[f"-IN-{x}"]] = float(values[f"-REQUIRED-{x}"])
                 with open("Icarus_Recipe/recipes.json", "w", encoding = "utf-8") as outfile:
                     json.dump(recipes, outfile, ensure_ascii=False, indent=4)
+                #layout = default_layout()
                 break
                 
 
@@ -53,11 +57,12 @@ def main() -> None:
 #for each key in recipe list, check if it's value is in the recipes. If so, then make the entry for that item. Otherwise, default format
 def get_craft_tree(tgt: dict, total: int = 1) -> dict():
     recipe_tree = dict()
+    recipe_tree[tgt] = dict()
     for k, v in recipes[tgt].items():
         if k in recipes:
-            recipe_tree[k] = get_craft_tree(k, total*v)
+            recipe_tree[tgt][k] = get_craft_tree(k, total*v)
         else:
-            recipe_tree[k] = v*int(total)
+            recipe_tree[tgt][k] = v*int(total)
     return recipe_tree
 
 def get_base_ingredients(tgt: dict) -> dict:
